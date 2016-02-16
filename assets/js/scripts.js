bfe7c81245aeff856e20e7eedd8bd05160cc58d5
jeffreyxiong <jeffxiong017@gmail.com>
2016-02-15 20:10:09 -0600

    front page design

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,48 @@
 // script.js
 $(document).ready(function() {
 
-	var bannerHeight = $('.banner').outerHeight();
+	// var declarations
+	var headingHeight = $('#heading').outerHeight();
 
-	// on scroll animations
-	$(window).scroll(function() {
-		// navbar animation
-		animateNavBar($(window));
-	}
-
-	// functions
+	//// functions
+	// nav bar animation
 	function animateNavBar(w) {
 		// check height
-		if (w.scrollTop() > bannerHeight) {
+		if (w.scrollTop() > headingHeight) {
 			// make position fixed
-
+			$('nav').css({'position':'fixed', 'background-color':'white'});
+			$('nav a').css({'color':'#443850', 'font-weight':'600'});
+			$('nav a').hover(
+				function() {
+					$(this).css({'background-color': '#E2DCEB'});
+				}, function() {
+					$(this).css({'background-color': 'transparent'});
+				}
+			);
+			$('#placeholder').css({'display':'block'});
 			// change color
+		} else {
+			$('nav').css({'position':'relative', 'background-color':'rgba(0, 0, 0, 0.6)'});
+			$('nav a').css({'color':'white', 'font-weight':'400'});
+			$('nav a').hover(
+				function() {
+					$(this).css({'background-color': 'rgba(0, 0, 0, 0.5)'});
+				}, function() {
+					$(this).css({'background-color': 'transparent'});
+				}
+			);
+			$('#placeholder').css({'display':'none'});
 		}
 	}
 
+	//// event listeners
+	
+	// on scroll animations
+	animateNavBar($(window));
 
-	// event listeners
+	$(window).scroll(function() {
+		// navbar animation
+		animateNavBar($(window));
+	});
 
-
-
-
-
-})
+});
