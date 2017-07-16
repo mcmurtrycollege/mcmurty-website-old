df9e61a8db6ff1c2af3fe40bd231265c42d0bb29
jeffreyxiong <jeffxiong017@gmail.com>
2017-07-15 23:40:48 -0700

    Masters -> Magisters

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,12 @@
 // script.js
 $(document).ready(function() {
 
-	// var declarations
+	//// var declarations
 	var headingHeight = $('.heading').outerHeight();
 	var colorDarkPurple = '#443850';
 	var colorPurple = '#8964A1';
-	$('.nav-dropdown .nav-link').click(function(e) {
-	    e.preventDefault();
-	});
+
+	//// setup
 
 	//// functions
 
@@ -32,35 +31,45 @@ $(document).ready(function() {
 		$(this).css({'color':colorDarkPurple});
 
 		var text = $(this).text();
-		var fellows = {"Masters":"masters", "Head Resident Fellows":"hrf", "2nd Floor RAs":"2ra", "3rd Floor RAs":"3ra", "College Coordinator":"cc"};
+		var fellows = {"Magisters":"magisters", "Head Resident Fellows":"hrf", "2nd Floor RAs":"2ra", "3rd Floor RAs":"3ra", "College Coordinator":"cc"};
 
 		$(".selected").removeClass("selected");
 		$(".fellow-" + fellows[text]).addClass("selected");
 	});
 
 	// nav bar helpers
+
+	$('.nav-dropdown .nav-link').click(function(e) {
+	    e.preventDefault();
+	});
+
+	$('#nav-icon-svg').click(function() {
+		$('nav').toggleClass('mobile-nav');
+		$('#nav-menu').toggleClass('mobile-nav-menu');
+	});
+
 	function animateNavColorPosition(position, bgColor, accentColor, fontColor, fontAccent, fontWeight, fontOldWeight) {
 
 		$('nav').css({'position': position, 'background-color': bgColor});
-			$('.nav-link').css({'color':fontColor, 'font-weight':fontWeight});
-			$('.nav-single').hover(
-				function() {
-					$(this).css({'background-color': accentColor});
-					$(this).css({'color':fontAccent, 'font-weight':fontWeight});
-				}, function() {
-					$(this).css({'background-color': 'transparent'});
-					$(this).css({'color':fontColor, 'font-weight':fontOldWeight});
-				}
-			);
-			$('.nav-dropdown').hover(
-				function() {
-					$(this).css({'background-color': accentColor});
-					$('.nav-link', this).css({'color':fontAccent, 'font-weight':fontWeight});
-				}, function() {
-					$(this).css({'background-color': 'transparent'});
-					$('.nav-link', this).css({'color':fontColor, 'font-weight':fontOldWeight});
-				}
-			);
+		$('.nav-link').css({'color':fontColor, 'font-weight':fontWeight});
+		$('.nav-single').hover(
+			function() {
+				$(this).css({'background-color': accentColor});
+				$(this).css({'color':fontAccent, 'font-weight':fontWeight});
+			}, function() {
+				$(this).css({'background-color': 'transparent'});
+				$(this).css({'color':fontColor, 'font-weight':fontOldWeight});
+			}
+		);
+		$('.nav-dropdown').hover(
+			function() {
+				$(this).css({'background-color': accentColor});
+				$('.nav-link', this).css({'color':fontAccent, 'font-weight':fontWeight});
+			}, function() {
+				$(this).css({'background-color': 'transparent'});
+				$('.nav-link', this).css({'color':fontColor, 'font-weight':fontOldWeight});
+			}
+		);
 	}
 
 	function animateDropDown(bgColor) {
@@ -100,11 +109,32 @@ $(document).ready(function() {
 	// on resize
 	// on scroll calls
 	$(window).scroll(function() {
-		// navbar animation
-		animateNavBar($(window));
+
+		// media query change
+		// function WidthChange(mq) {
+		//   	if (mq.matches) {
+		//     	// window width is at most 920px
+		//     	alert("hello")
+		//   	} else {
+		//     	// window width is more than 920px
+		//   	}
+		// }
+		if (!window.matchMedia("(max-width: 920px), (max-device-width: 920px)").matches) {
+			// navbar animation
+			animateNavBar($(window));
+		}
+		
+	});
+
+	$(window).resize(function() {
+		if (!window.matchMedia("(max-width: 920px), (max-device-width: 920px)").matches) {
+			// navbar animation
+			animateNavBar($(window));
+		}
 	});
 
 	// on load calls
-	animateNavBar($(window));
-
+	if (!window.matchMedia("(max-width: 920px), (max-device-width: 920px)").matches) {
+		animateNavBar($(window));
+	}
 });
